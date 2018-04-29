28ed8a832a0c72ce64f183a53311e3ba3cd70a22
Max-Emilien Dauvert-Latre <max.emilien96perso@hotmail.com>
2018-04-30 00:17:41 +0200

    Edit languages display buttons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {TranslateService} from 'ng2-translate';
+import { TranslateService } from 'ng2-translate';
 
 const defaultLanguage = "en";
-const additionalLanguages = ["fr","en"];
-const languages: string[] = 
-[defaultLanguage].concat(additionalLanguages);
+const additionalLanguages = ["fr", "en"];
+const languages: string[] =
+    [defaultLanguage].concat(additionalLanguages);
 
 @Component({
     selector: 'navbar',
@@ -14,9 +14,9 @@ const languages: string[] =
 
 export class NavbarComponent implements OnInit {
     constructor(private translate: TranslateService) {
-			this.translate.setDefaultLang(defaultLanguage);
+        this.translate.setDefaultLang(defaultLanguage);
         this.translate.addLangs(additionalLanguages);
- 
+
         let initLang = this.translate.getBrowserLang();
         if (languages.indexOf(initLang) === -1) {
             initLang = defaultLanguage;
@@ -24,10 +24,12 @@ export class NavbarComponent implements OnInit {
         this.translate.use(initLang);
     }
 
+    defaultHidden : boolean = true;
 
-  changeLanguage(lang){
-	this.translate.use(lang);
-  }
+changeLanguage(lang){
+    this.defaultHidden = lang == defaultLanguage;
+    this.translate.use(lang);
+}
 
-    ngOnInit() { }
+ngOnInit() { }
 }
